2d8bcf61ac47595bd317bd4ef3b7430d9a8d74f7
SEFI2 <kadirpili@gmail.com>
2020-04-13 22:03:01 +0300

    divide client and server

--- server/app.js
+++ server/app.js
@@ -1,55 +1,55 @@
 /** require dependencies */
-const express = require("express")
-const routes = require('./routes/')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const cloudinary = require('cloudinary')
-const firebase = require('./firebase/auth')
-const usersRouter = require('./routes/users')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cloudinary = require('cloudinary');
+const routes = require('./routes');
+const firebase = require('./firebase/auth');
+const usersRouter = require('./routes/users');
 
-const app = express()
-const router = express.Router()
-const url = process.env.MONGODB_URI || "mongodb://localhost:27017/medium"
+const app = express();
+const router = express.Router();
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/medium';
 
 /** configure cloudinary */
 cloudinary.config({
-    cloud_name: 'chidumennamdi',
-    api_key: '',
-    api_secret: ''
-})
+  cloud_name: 'chidumennamdi',
+  api_key: '',
+  api_secret: '',
+});
 
 /** connect to MongoDB datastore */
 try {
-    mongoose.connect(url, {
-        //useMongoClient: true
-    })    
+  mongoose.connect(url, {
+    // useMongoClient: true
+  });
 } catch (error) {
-    
+
 }
 
-let port = 5000 || process.env.PORT
+const port = 5000 || process.env.PORT;
 
 /** set up routes {API Endpoints} */
-routes(router)
+routes(router);
 
 /** set up middlewares */
-app.use(cors())
-app.use(bodyParser.json())
-app.use(helmet())
-//app.use('/static',express.static(path.join(__dirname,'static')))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
+// app.use('/static',express.static(path.join(__dirname,'static')))
 
 // Auth checker middleware
-app.use(firebase.firebaseAuthenticationMiddleware)
-app.use(firebase.firebaseAuthorizationMiddleware)
-app.use(firebase.firebaseAddUserMiddleware)
+app.use(firebase.firebaseAuthenticationMiddleware);
+app.use(firebase.firebaseAuthorizationMiddleware);
+app.use(firebase.firebaseAddUserMiddleware);
 
 
-app.use('/api', router)
-app.use('/users', usersRouter)
+app.use('/api', router);
+app.use('/users', usersRouter);
 
 /** start server */
 app.listen(port, () => {
-    console.log(`Server started at port: ${port}`);
+  console.log(`Server started at port: ${port}`);
 });
--- server/controllers/user.ctrl.js
+++ server/controllers/user.ctrl.js
@@ -1,52 +1,54 @@
 /** */
-const User = require('./../models/User')
-const Article = require('./../models/Article')
+const { query } = require('express-validator/check');
+const User = require('../models/User');
+const Article = require('../models/Article');
 
 module.exports = {
-    addUser: (req, res, next) => {
-        new User(req.body).save((err, newUser) => {
-            if (err)
-                res.send(err)
-            else if (!newUser)
-                res.send(400)
-            else
-                res.send(newUser)
-            next()
-        });
-    },
-    getUser: (req, res, next) => {
-        User.findById(req.params.id).then
-        /*populate('following').exec*/((err, user)=> {
-            if (err)
-                res.send(err)
-            else if (!user)
-                res.send(404)
-            else
-                res.send(user)
-            next()            
-        })
-    },
-    /**
+  validate: (method) => {
+    switch (method) {
+      case 'getUsers': {
+        return [
+          query('limit').optional().isInt().toInt(),
+          query('skip').optional().isInt().toInt(),
+        ];
+      }
+    }
+  },
+  getUsers: async (req, res, next) => {
+    User.find().limit(req.query.limit).skip(req.query.skip).then((err, users) => {
+      if (err) res.send(err);
+      else res.send(users);
+      next();
+    })
+  }
+  addUser: (req, res, next) => {
+    new User(req.body).save((err, newUser) => {
+      if (err) res.send(err);
+      else if (!newUser) res.send(400);
+      else res.send(newUser);
+      next();
+    });
+  },
+  getUser: (req, res, next) => {
+    User.findById(req.params.id).then((err, user) => {
+      if (err) res.send(err);
+      else if (!user) res.send(404);
+      else res.send(user);
+      next();
+    });
+  },
+  /**
      * user_to_follow_id, user_id
      */
-    followUser: (req, res, next) => {
-        User.findById(req.body.id).then((user) => {
-            return user.follow(req.body.user_id).then(() => {
-                return res.json({msg: "followed"})
-            })
-        }).catch(next)
-    },
-    getUserProfile: (req, res, next) => {
-        User.findById(req.params.id).then
-        ((_user) => {
-            return User.find({'following': req.params.id}).then((_users)=>{
-                _users.forEach((user_)=>{
-                    _user.addFollower(user_)
-                })
-                return Article.find({'author': req.params.id}).then((_articles)=> {
-                    return res.json({ user: _user, articles: _articles })
-                })
-            })
-        }).catch((err)=>console.log(err))
-    }
-}
+  followUser: (req, res, next) => {
+    User.findById(req.body.id).then((user) => user.follow(req.body.user_id).then(() => res.json({ msg: 'followed' }))).catch(next);
+  },
+  getUserProfile: (req, res, next) => {
+    User.findById(req.params.id).then((_user) => User.find({ following: req.params.id }).then((_users) => {
+      _users.forEach((user_) => {
+        _user.addFollower(user_);
+      });
+      return Article.find({ author: req.params.id }).then((_articles) => res.json({ user: _user, articles: _articles }));
+    })).catch((err) => console.log(err));
+  },
+};
